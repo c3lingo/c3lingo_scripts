d34c97297c4e413229ef9d065a84b0dc70e836fa
Adrien Beaucreux <informancer@web.de>
2021-12-29 11:52:06 +0100

    Finished tallying up hours

--- tallyUpHours.py
+++ tallyUpHours.py
@@ -1,6 +1,9 @@
-from datetime import timedelta
-
-
 def tally_up(talks):
     scoreboard = {}
+    for talk in talks:
+        for translator in talk.translators:
+            if translator in scoreboard:
+                scoreboard[translator] = scoreboard[translator] + talk.duration
+            else:
+                scoreboard[translator] = talk.duration
     return scoreboard
--- test_tally_up.py
+++ test_tally_up.py
@@ -9,3 +9,54 @@ def test_tally_empty_list():
     scoreboard = tally_up([])
     assert_that(scoreboard, equal_to({}))
 
+
+def test_tally_one_talk():
+    scoreboard = tally_up([Talk(title='',
+                                date=1,
+                                time='',
+                                duration=timedelta(hours=0, minutes=15),
+                                place='',
+                                speaker='',
+                                language='',
+                                fahrplan_url='',
+                                translations=(),
+                                translators=('one', 'two'))])
+    assert_that(scoreboard.keys(), contains_exactly('one', 'two'))
+    assert_that(scoreboard['one'], equal_to(timedelta(hours=0, minutes=15)))
+
+
+def test_tally_multiple_talks():
+    scoreboard = tally_up([Talk(title='',
+                                date=1,
+                                time='',
+                                duration=timedelta(hours=0, minutes=15),
+                                place='',
+                                speaker='',
+                                language='',
+                                fahrplan_url='',
+                                translations=(),
+                                translators=('one', 'two')),
+                           Talk(title='',
+                                date=1,
+                                time='',
+                                duration=timedelta(hours=1, minutes=0),
+                                place='',
+                                speaker='',
+                                language='',
+                                fahrplan_url='',
+                                translations=(),
+                                translators=('one', 'three')),
+                           Talk(title='',
+                                date=1,
+                                time='',
+                                duration=timedelta(hours=0, minutes=30),
+                                place='',
+                                speaker='',
+                                language='',
+                                fahrplan_url='',
+                                translations=(),
+                                translators=('two',))])
+    assert_that(scoreboard.keys(), contains_exactly('one', 'two', 'three'))
+    assert_that(scoreboard['one'], equal_to(timedelta(hours=1, minutes=15)))
+    assert_that(scoreboard['two'], equal_to(timedelta(hours=0, minutes=45)))
+    assert_that(scoreboard['three'], equal_to(timedelta(hours=1, minutes=00)))
