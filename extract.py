dbcdde05dfe40aa4fdabdb23a1fdfc975627fa71
Adrien Beaucreux <informancer@web.de>
2021-12-29 08:05:35 +0100

    reuse from toot translation

---
+++ extract.py
@@ -0,0 +1,120 @@
+import re
+from collections import namedtuple
+
+
+def format_toot(format, title, date, time, place):
+    return format.format(title, date, time, place)
+
+
+Talk = namedtuple('Talk', ['title', 'date', 'time', 'place', 'translations'])
+
+
+TRANSLATION_RE = r'^\s*→\s*(?P<lang>[a-z]{2})\s*:'
+
+def extract_talks(day, content):
+    new_talk = False
+    current_talk = ''
+    the_date = day
+    the_time = ''
+    the_place = ''
+    translations = ()
+
+    for line in content:
+        # This marks the start of a talk in the plan
+        if line.startswith('#'):
+            new_talk = True
+            continue
+
+        if new_talk and line.startswith('['):
+            _, the_time, _, the_place = line.strip().split()
+            continue
+
+        if new_talk and line.startswith('[') is False:
+            current_talk = line.strip()
+            new_talk = False
+            continue
+
+        if current_talk:
+            match = re.match(TRANSLATION_RE, line)
+            if match:
+                translations += (match.group('lang'),)
+
+        # An empty line is the end of a talk block
+        if not line.strip():
+            yield Talk(current_talk, the_date, the_time, the_place, translations)
+            current_talk = ''
+            the_time = ''
+            the_place = ''
+            translations = ()
+            continue
+
+
+def main():
+    new_talk = False
+    current_talk= ''
+    the_date = '3'
+    the_time = ''
+    the_place = ''
+
+    for line in open('/home/informancer/Downloads/36c3-day3-latest.txt'):
+
+        # This marks the start of a talk in the plan
+        if line.startswith('#'):
+            #print(line.strip())
+            new_talk = True
+            continue
+
+        if new_talk and line.startswith('['):
+            _, the_time, _, the_place = line.strip().split()
+            continue
+
+        if new_talk and line.startswith('[') is False:
+            #print(line.strip())
+            current_talk = line.strip()
+            new_talk = False
+            continue
+
+        # The talk has a marker for a french translation
+        if current_talk and re.match(r'^\s*→\s*fr\s*:', line):
+            # there is more than one translator
+            print(format_toot('La présentation "{}" (jour {} {} à {}) sera traduite en Francais.',
+                              current_talk, the_date, the_time, the_place))
+            continue
+
+        # The talk has a marker for an english translation
+        if current_talk and re.match(r'^\s*→\s*en\s*:', line):
+            # there is more than one translator
+            print(format_toot('The talk "{}" (day {} {} in {}) will be translated in english.',
+                              current_talk, the_date, the_time, the_place))
+            continue
+
+        # The talk has a marker for a german translation
+        if current_talk and re.match(r'^\s*→\s*de\s*:', line):
+            # there is more than one translator
+            print(format_toot('Der Vortrag "{}" (Tag {} {} im Raum {}) wird auf Deutsch übersetzt.',
+                              current_talk, the_date, the_time, the_place))
+
+        # The talk has a marker for a polish translation
+        if current_talk and re.match(r'^\s*→\s*pl\s*:', line):
+            # there is more than one translator
+            print(format_toot('Wykład "{}" (dzień {}, {} w sali {}) będzie tłumaczony na polski.',
+                              current_talk, the_date, the_time, the_place))
+
+        # The talk has a marker for a spanish translation
+        if current_talk and re.match(r'^\s*→\s*es\s*:', line):
+            # there is more than one translator
+            print(format_toot('La charla "{}" (día {}, a las {}, salón {}) será traducida al español.',
+                              current_talk, the_date, the_time, the_place))
+
+        # The talk has a marker for a russian translation
+        if current_talk and re.match(r'^\s*→\s*ru\s*:', line):
+            # there is more than one translator
+            print(format_toot('Лекция «{}» (день {} {} в зале {}) будет переведена на английский',
+                              current_talk, the_date, the_time, the_place))
+        
+        # An empty line is the end of a talk block
+        if not line.strip():
+            current_talk = ''
+            the_time = ''
+            the_place = ''
+            continue
---
+++ test_talk_generator.py
@@ -0,0 +1,57 @@
+from extract import extract_talks, TRANSLATION_RE
+from hamcrest import assert_that, equal_to, contains
+import re
+
+
+def test_translation_re():
+    result = re.match(TRANSLATION_RE, '')
+    assert_that(result, equal_to(None))
+    result = re.match(TRANSLATION_RE, '→ ')
+    assert_that(result, equal_to(None))
+    result = re.match(TRANSLATION_RE, '→ f: blah')
+    assert_that(result, equal_to(None))
+    result = re.match(TRANSLATION_RE, '→ french: blah')
+    assert_that(result, equal_to(None))
+    result = re.match(TRANSLATION_RE, '→ fr: blah')
+    assert_that(result, not equal_to(None))
+    assert_that(result.group('lang'), equal_to('fr'))
+    result = re.match(TRANSLATION_RE, '→ en: blah')
+    assert_that(result, not equal_to(None))
+    assert_that(result.group('lang'), equal_to('en'))
+    result = re.match(TRANSLATION_RE, '→ ru: blah')
+    assert_that(result, not equal_to(None))
+    assert_that(result.group('lang'), equal_to('ru'))
+
+
+def test_talk_no_translation():
+    test_content = ['### #1  ',
+                    '[de] **10:45** +00:15, [r3s - Monheim/Rhein](https://meet.ffmuc.net/rc3r3sc3lingolowlatency)  '
+                    '**R3S Opening** (Lightning Talk)  '
+                    'heyhej  '
+                    'Fahrplan: https://pretalx.c3voc.de/rc3-2021-r3s/talk/QED93K/  '
+                    'Slides (if available): https://speakers.c3lingo.org/talks/ea266d48-e185-5dbe-90dd-801a8fbe6ecc/  '
+                    '→ en: one',
+                    '→ fr: two, three',]
+    talks = list(extract_talks(3, test_content))
+    assert_that(len(talks), equal_to(1))
+    talk = talks[0]
+    assert_that(talk.title, equal_to('R3S Opening'))
+    assert_that(talk.date, equal_to(3))
+    assert_that(talk.time, equal_to('10:45'))
+    assert_that(talk.place, equal_to('Ada'))
+
+
+def test_talk_translations():
+    test_content = ['#1',
+                    '[de] 11:30 +01:00, Ada',
+                    'Von Menschen radikalisiert: Über Rassismus im Internet',
+                    'Michael Kreil',
+                    '→ en: one, two',
+                    '→ ru: three, four',
+                    '']
+    talks = list(extract_talks(3, test_content))
+    assert_that(len(talks), equal_to(1))
+    talk = talks[0]
+    assert_that(len(talk.translations), equal_to(2))
+    print(talk.translations)
+    assert_that(talk.translations, contains('en', 'ru'))
